5a69b663c7eedd86c14ebfa5336e37d94ab23057
Jeremy <jeremy.sinitean@gmail.com>
2012-02-22 20:07:07 -0600

    all add

--- db/migrate/20120223005939_add_price_sections.rb
+++ db/migrate/20120223005939_add_price_sections.rb
@@ -1,9 +1,9 @@
 class AddPriceSections < ActiveRecord::Migration
   def up
-    add_column("no_of_tickets", "username", :string, :limit => 25)
+    add_column("sections", "no_of_tickets", :string, :limit => 25)
   end
 
   def down
-    remove_column("no_of_tickets", "username")
+    remove_column("sections", "no_of_tickets")
   end
 end
